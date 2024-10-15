2ce4b187281cea8981b743f2cbdc7508a189ccdc
John Hash <jhash@esri.com>
2024-10-15 13:41:58 -0700

    cleaning up gitignore

--- Samples/custom-data-feeds/yelp-full-fetch-ready-only-points/providers/yelp-data-provider/src/model.js
+++ Samples/custom-data-feeds/yelp-full-fetch-ready-only-points/providers/yelp-data-provider/src/model.js
@@ -13,7 +13,60 @@ Model.prototype.getData = function(req, callback) {
     })
     .then(response => {
       geojson = parseGeoJSON(response.jsonBody);
-      geojson.metadata = { name: [host, "_in_", id].join("")};
+      geojson.metadata = { 
+        name: [host, "_in_", id].join(""),
+        labelingInfo: [
+          {
+            "symbol": {
+              "kerning": true,
+              "color": [214, 75, 75, 255],
+              "yoffset": 0,
+              "xoffset": 0,
+              "haloColor": [211, 211, 211, 255],
+              "rotated": false,
+              "type": "esriTS",
+              "horizontalAlignment": "center",
+              "haloSize": 1,
+              "angle": 0,
+              "text": "",
+              "verticalAlignment": "baseline",
+              "font": {
+                "size": 12.75,
+                "family": "Arial"
+              }
+            },
+            "maxScale": 0,
+            "repeatLabel": true,
+            "where": "category = 'Cocktail Bars'",
+            "minScale": 0,
+            "labelExpressionInfo": {
+              "expression": "$feature[\"category\"]"
+            },
+            "labelExpression": "[category]",
+            "labelPlacement": "esriServerPolygonPlacementAlwaysHorizontal"
+          }
+        ],
+        renderer: {
+          "type": "simple",
+          "symbol": {
+            "type": "esriSMS",
+            "style": "esriSMSCircle",
+            "color": [255, 0, 0, 255],
+            "size": 10,
+            "angle": 0,
+            "xoffset": 0,
+            "yoffset": 0,
+            "outline": {
+              "color": [0, 0, 0, 255],
+              "width": 1
+            }
+          },
+          "label": "",
+          "description": "",
+          "rotationType": "geographic",
+          "rotationExpression": "[Rotation] * 2"
+        }
+      };
       callback(null, geojson);
     })
     .catch(e => {
