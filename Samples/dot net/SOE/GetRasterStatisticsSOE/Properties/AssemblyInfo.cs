ddbfa9e961e123bb16de140775c6c56d2c749dc3
fan11765 <fzeng@esri.com>
2024-11-08 15:15:55 -0800

    updated samples add two image service samples

---
+++ Samples/dot net/SOE/GetRasterStatisticsSOE/Properties/AssemblyInfo.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GetRasterStatistics")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ESRI")]
+[assembly: AssemblyProduct("GetRasterStatistics")]
+[assembly: AssemblyCopyright("Copyright © ESRI 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("041edd44-4a33-4fb8-909d-1bc6ffb7b163")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+
+[assembly: ESRI.Server.SOESupport.AddInPackage("GetRasterStatistics", "df6efd2a-98d6-4301-8b38-49a140c5d4ba",
+    Author = "pank9616",
+    Company = "ESRI",
+    Date = "2/28/2024 8:22:11 PM",
+    Description = "",
+    TargetProduct = "Server",
+    TargetVersion = "11.3",
+    Version = "1.0")]
