d6df6f1bf6301c563c86fe88e7e81ecd5550c4af
John Hash <jhash@esri.com>
2025-02-18 11:13:32 -0800

    controller model and perform edits files updated

--- Samples/custom-data-feeds/mongodb-editing-points/providers/editable-mongodb-points/src/model.js
+++ Samples/custom-data-feeds/mongodb-editing-points/providers/editable-mongodb-points/src/model.js
@@ -1,16 +1,184 @@
-function Model (cdfServer) {}
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const config = require('config');
+const { performEdits } = require('./perform-edits');
 
-// A Model is a javascript function that encapsulates custom data access code.
-// Each model should have a getData() function to fetch the geo data
-// and format it into a geojson
-Model.prototype.getData = function (req, callback) {
-  const geojson = {
-    type: 'FeatureCollection',
-    features: []
+class Model {
+  #client;
+  #databaseLookup;
+  #definedCollectionsOnly;
+  #logger;
+
+  constructor(
+    { logger },
+    { connectString, databases, definedCollectionsOnly } = {},
+  ) {
+    this.#logger = logger;
+    const databaseUri = connectString || config?.editable_mongodb_points?.connectString;
+    this.#client = new MongoClient(databaseUri, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      },
+    });
+    this.#databaseLookup = databases || config?.editable_mongodb_points?.databases || {};
+    this.#definedCollectionsOnly =
+      definedCollectionsOnly ||
+      config?.editable_mongodb_points?.definedCollectionsOnly ||
+      false;
   }
 
-  // the callback function expects a geojson for its second parameter
-  callback(null, geojson)
+  async editData(req) {
+
+    // assign database and collection name from path parameters
+    const databaseName = req.params.host;
+    const collectionName = req.params.id;
+
+    // add logic to normalize layer-level or service-level requests
+    const extractedEdits = normalizeRequestedEdits(req.body);
+
+    const database  = this.#client.db(databaseName);
+    const collection = database.collection(collectionName);
+
+    let applyEditsResponse = {};  // initialize the response object
+
+    // call the necessary functions to handled the edits
+    applyEditsResponse = await performEdits(collection, extractedEdits.edits);
+
+    // check if the response should be an object(layer-level) or an array(service-level)
+    if (extractedEdits.editLevel === 'service') {
+      applyEditsResponse.id = extractedEdits.layer;
+      return [applyEditsResponse];
+
+    }
+
+    return applyEditsResponse;
+
+  }
+
+  // this a very basic getData function that operates as full-fetch
+  async getData(req) {
+
+    const databaseName = req.params.host;
+    const collectionName = req.params.id;
+
+    try {
+
+      const database  = this.#client.db(databaseName);
+      const collection = database.collection(collectionName);
+
+      const results = await fetchDocs(collection);
+     
+      const geojson = convertDocsToGeoJSON(results, 'location');
+
+      geojson.metadata
+
+      return {...geojson, metadata: { 
+        idField: 'alternateID', 
+        name: 'Fires',
+        templates: [
+          {
+           "name": "Edit MongoDB Fires",
+           "description": "Template for editing fire data features",
+           "drawingTool": "esriFeatureEditToolPoint",
+           "prototype": {
+            "attributes": {}
+           }
+          }
+         ],
+        fields: [
+          {
+            "name": "_id",
+            "type": "string",
+            "alias": "mongoID",
+            "length": 128,
+            "editable": false
+          },
+          {
+            "name": "alternateID",
+            "type": "bigInteger",
+            "alias": "alternateID",
+            "editable": false
+          },
+          {
+              "name": "fireId",
+              "type": "string",
+              "alias": "fireId",
+              "length": 128,
+              "editable": true
+          },
+          {
+              "name": "fireName",
+              "type": "string",
+              "alias": "fireName",
+              "length": 128,
+              "editable": true
+          },
+          {
+              "name": "fireType",
+              "type": "string",
+              "alias": "fireType",
+              "length": 128,
+              "editable": true
+          },
+          {
+              "name": "acres",
+              "type": "string",
+              "alias": "acres",
+              "length": 128,
+              "editable": true
+          }
+        ]
+      }};
+
+    } catch (error) {
+      this.#logger.warning(error)
+      return error
+    }
+    
+  }
 }
 
-module.exports = Model
+async function fetchDocs(items) {
+  const results = await items.find({}).toArray();
+  return results
+}
+
+function convertDocsToGeoJSON(docs, geometryField) {
+  const features = docs.map((record) => {
+    const { [geometryField]: geometry, ...properties } = record;
+    return { geometry, properties };
+  });
+
+  return {
+    type: 'FeatureCollection',
+    features,
+  };
+}
+
+function normalizeRequestedEdits(body) {
+  let edits = {};
+  // if it service-level, just return the object
+  if(body.edits) { 
+  
+    return {
+      edits: body.edits[0],
+      editLevel : 'service',
+      layer: body.edits[0].id
+    }
+  // handle the layer level request
+  } else {
+    if(body.adds) edits.adds = body.adds;
+    if(body.updates) edits.updates = body.updates;
+    if(body.deletes) {
+      let deletesArray = [body.deletes];
+      edits.deletes = deletesArray;
+    }
+    return {
+      edits: edits,
+      editLevel: 'layer'
+    }
+  }
+}
+
+module.exports = Model;
